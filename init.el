767e052df75f44cd2e0942878efe44bef727a138
IngeniousTechie <mukesh@ingenioustechie.com>
2015-06-14 19:39:34 +0530

    Removed startup splash

---
+++ init.el
@@ -0,0 +1,11 @@
+
+
+(menu-bar-mode -1)
+
+;; no startup msg  
+(setq inhibit-startup-message t)        ; Disable startup message 
+
+;; This mode is for autocomplete file loading file 
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
