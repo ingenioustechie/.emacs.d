38c06b2d8141e9f25b9fe903d985abdafab731c8
IngeniousTechie <mukesh@ingenioustechie.com>
2015-06-15 11:55:32 +0530

    Added gitignore for ac-comphist.dat

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 
 
+
+
+;; Using marmalade for packages
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
 (menu-bar-mode -1)
 
 ;; no startup msg  
@@ -9,3 +16,14 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+
+
+;; Standard Jedi.el setting
+;; (require 'epc)
+;; (setq jedi:setup-keys t)
+;; (setq jedi:complete-on-dot t)
+;; (setq jedi:server-command '("python" "/home/mak/.emacs.d/elpa/jedi-0.1.2/jediepcserver.py"))
+;; (setq jedi:tooltip-method '(pos-tip))
+;; (autoload 'jedi:setup "jedi" nil t)
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:complete-on-dot t)                 ; optional
